55d8437eef06ddd64046c6d6b5002a5f0e07b99a
kerkh <kerkh@192.168.0.152>
2020-02-26 14:48:51 +0100

    DoublePoint and IntPoint, uncomplete documentation

--- OGPProject/src/drawit/DoublePoint.java
+++ OGPProject/src/drawit/DoublePoint.java
@@ -1,28 +1,66 @@
 package drawit;
 
+/**
+ * An immutable abstraction for a point in the two-dimensional plane with double coordinates.
+ */
 public class DoublePoint {
 	
+	private double x;
+	private double y;
+	
+	/**
+	 * Initializes this point with the given coordinates.
+	 */
 	public DoublePoint(double x, double y) {
-		throw new RuntimeException("Not yet implemented");
+		this.x = x;
+		this.y = y;
 	}
 	
+	/**
+	 * Returns an DoublePoint object representing the point obtained by displacing this point by the given vector.
+	 * 
+	 * @post The result is a vector with its x-coordinate being the sum of the x-coordinates of both points
+	 *    | result.getX() == this.getX() + other.getX()
+	 * @post The result is a vector with its y-coordinate being the sum of the y-coordinates of both points
+	 *    | result.getY() == this.getY() + other.getY()
+	 */
 	public DoublePoint plus(DoubleVector other) {
-		throw new RuntimeException("Not yet implemented");		
+		return new DoublePoint(x + other.getX(), y + other.getY());	
 	}
 	
+	/**
+	 * Returns an DoubleVector object representing the displacement from other to this.
+	 * 
+	 * @post The result is a vector with its x-coordinate being the subtraction of the x-coordinate of the given point from that of this point.
+	 *    | result.getX() == this.getX() - other.getX()
+	 * @post The result is a vector with its y-coordinate being the subtraction of the y-coordinate of the given point from that of this point.
+	 *    | result.getY() == this.getY() - other.getY()
+	 */
 	public DoubleVector minus(DoublePoint other) {
-		throw new RuntimeException("Not yet implemented");
+		return new DoubleVector(x - other.getX(), y - other.getY());	
 	}	
 	
+	/**
+	 * Returns an IntPoint object whose coordinates are obtained by rounding the coordinates of this point to the nearest integer.
+	 * 
+	 * @post The result is an IntPoint whose coordinates are rounded to the nearest integer.
+	 *    | result == new IntPoint((int) Math.round(this.getX()), (int) Math.round(this.getY()))
+	 */
 	public IntPoint round() {
-		throw new RuntimeException("Not yet implemented");
+		return new IntPoint((int) Math.round(x), (int) Math.round(y));
 	}
 	
+	/**
+	 * Returns this point's X coordinate.
+	 */
 	public double getX() {
-		throw new RuntimeException("Not yet implemented");		
+		return x;	
 	}
 	
+	/**
+	 * Returns this point's Y coordinate.
+	 */
 	public double getY() {
-		throw new RuntimeException("Not yet implemented");
+		return y;
 	}
 }
--- OGPProject/src/drawit/IntPoint.java
+++ OGPProject/src/drawit/IntPoint.java
@@ -1,40 +1,112 @@
 package drawit;
 
+/**
+ * An immutable abstraction for a point in the two-dimensional plane with int coordinates.
+ */
 public class IntPoint {
 	
+	private int x;
+	private int y;
+	
+	/**
+	 * Initializes this point with the given coordinates.
+	 */
 	public IntPoint(int x, int y) {
-		throw new RuntimeException("Not yet implemented");
+		this.x = x;
+		this.y = y;
 	}
 	
+	/**
+	 * Returns true iff the open line segment ab intersects the open line segment cd.
+	 * 
+	 * @pre The line segments have at most one point in common.
+	 *     | 0 <= x
+	 */
 	public static boolean lineSegmentsIntersect(IntPoint a, IntPoint b, IntPoint c, IntPoint d) {
-		throw new RuntimeException("Not yet implemented");
+		IntVector ab = new IntVector(a.getX() - b.getX(), a.getY() - b.getY());
+		IntVector ac = new IntVector(a.getX() - c.getX(), a.getY() - c.getY());
+		IntVector ad = new IntVector(a.getX() - d.getX(), a.getY() - d.getY());
+		int val = (int) (Math.signum(ac.crossProduct(ab)) * (Math.signum(ad.crossProduct(ab))));
+		
+		if(val < 0) {
+			return true;
+		}
+		return false;
 	}	
 	
+	/**
+	 * Returns true iff this point is on open line segment bc. An open line segment does not include its endpoints.
+	 */
 	public boolean isOnLineSegment(IntPoint b, IntPoint c) {
-		throw new RuntimeException("Not yet implemented");
+		IntVector ba = new IntVector(b.getX() - x, b.getY() - y);
+		IntVector bc = new IntVector(b.getX() - c.getX(), b.getY() - c.getY());
+		
+		if(ba.isCollinearWith(bc)) {
+			return false;
+		} else if(0 < ba.crossProduct(bc) && ba.crossProduct(bc) < bc.crossProduct(bc) ) {
+			return true;
+		}
+		return false;
 	}
 	
+	/**
+	 * Returns true if this point has the same coordinates as the given point; returns false otherwise.
+	 * 
+	 * 
+	 */
 	public boolean equals(IntPoint other) {
-		throw new RuntimeException("Not yet implemented");
+		if(x == other.getX() && y == other.getY()) {
+			return true;
+		}
+		return false;
 	}
 	
+	/**
+	 * Returns an IntPoint object representing the point obtained by displacing this point by the given vector.
+	 * @post The result is a vector with its x-coordinate being the sum of the x-coordinates of both points
+	 *    | result.getX() == this.getX() + vector.getX()
+	 * @post The result is a vector with its y-coordinate being the sum of the y-coordinates of both points
+	 *    | result.getY() == this.getY() + vector.getY()
+	 */
 	public IntPoint plus(IntVector vector) {
-		throw new RuntimeException("Not yet implemented");		
+		return new IntPoint(x + vector.getX(), y + vector.getY());		
 	}
 	
+	/**
+	 * Returns an IntVector object representing the displacement from other to this.
+	 * 
+	 * @post The result is a vector with its x-coordinate being the subtraction of the x-coordinate of the given point from that of this point.
+	 *    | result.getX() == this.getX() - other.getX()
+	 * @post The result is a vector with its y-coordinate being the subtraction of the y-coordinate of the given point from that of this point.
+	 *    | result.getY() == this.getY() - other.getY()
+	 */
 	public IntVector minus(IntPoint other) {
-		throw new RuntimeException("Not yet implemented");
+		return new IntVector(x - other.getX(), y - other.getY());
 	}	
 	
+	/**
+	 * Returns a DoublePoint object that represents the same 2D point represented by this IntPoint object.
+	 * 
+	 * @post The x-coordinate of the result is the same as this x-coordinate
+	 *    | result.getX() == this.getX()
+	 * @post The y-coordinate of the result is the same as this y-coordinate
+	 *    | result.getY() == this.getY()
+	 */
 	public DoublePoint asDoublePoint() {
-		throw new RuntimeException("Not yet implemented");
+		return new DoublePoint((double) x, (double) y);
 	}
 	
+	/**
+	 * Returns this point's X coordinate.
+	 */
 	public int getX() {
-		throw new RuntimeException("Not yet implemented");
+		return x;
 	}
 	
+	/**
+	 * Returns this point's Y coordinate.
+	 */
 	public int getY() {
-		throw new RuntimeException("Not yet implemented");
+		return y;
 	}
 }
