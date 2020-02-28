565ccbf4609cf6b3ed71c0e8487ad05b34122ae2
kerkh <kerkh@192.168.0.152>
2020-02-28 22:04:03 +0100

    Added most of PointArrays, added tests for IntPoint and DoublePoint

--- OGPProject/src/drawit/PointArrays.java
+++ OGPProject/src/drawit/PointArrays.java
@@ -7,18 +7,57 @@ public class PointArrays {
 	}
 	
 	public static IntPoint[] copy(IntPoint[] points) {
-		throw new RuntimeException("Not yet implemented");
+		IntPoint[] newArray = new IntPoint[points.length];
+		
+		for(int i = 0; i < points.length; i++) {
+			newArray[i] = points[i];
+		}
+		return newArray;
 	}
 	
 	public static IntPoint[] update(IntPoint[] points, int index, IntPoint value) {
-		throw new RuntimeException("Not yet implemented");
+		IntPoint[] newArray = new IntPoint[points.length];
+		
+		for(int i = 0; i < points.length; i++) {
+			if(i == index) {
+				newArray[i] = value;
+			} else {
+				newArray[i] = points[i];
+			}
+		}
+		return newArray;
 	}
 	
 	public static IntPoint[] insert(IntPoint[] points, int index, IntPoint point) {
-		throw new RuntimeException("Not yet implemented");
+		IntPoint[] newArray = new IntPoint[points.length + 1];
+		boolean insertDone = false;
+		
+		for(int i = 0; i < points.length + 1; i++) {
+			if(i == index) {
+				newArray[i] = point;
+				insertDone = true;
+			} else if(!insertDone) {
+				newArray[i] = points[i];
+			} else {
+				newArray[i] = points[i - 1];
+			}
+		}
+		return newArray;
 	}
 	
 	public static IntPoint[] remove(IntPoint[] points, int index) {
-		throw new RuntimeException("Not yet implemented");
+		IntPoint[] newArray = new IntPoint[points.length - 1];
+		boolean removeDone = false;
+		
+		for(int i = 0; i < points.length; i++) {
+			if(i == index) {
+				removeDone = true;
+			} else if(!removeDone) {
+				newArray[i] = points[i];
+			} else {
+				newArray[i - 1] = points[i];
+			}
+		}
+		return newArray;
 	}	
 }
