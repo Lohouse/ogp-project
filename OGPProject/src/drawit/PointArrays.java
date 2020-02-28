7742e5f0f1954b97fe79b288cf9daaf34f0a0db8
Alexander Lohaus <alexander.lohaus@gmail.com>
2020-02-28 23:39:45 +0100

    Implement PointArrays.definesProperPolygon(IntPoint[] points)

--- OGPProject/src/drawit/PointArrays.java
+++ OGPProject/src/drawit/PointArrays.java
@@ -3,7 +3,46 @@ package drawit;
 public class PointArrays {
 	
 	public static String checkDefinesProperPolygon(IntPoint[] points) {
-		throw new RuntimeException("Not yet implemented");
+		if (points.length < 3) {
+			return "Less than 3 vertices";
+		}
+		
+		for (int i = 0; i < points.length; i++) {
+			int j = (i + 1) % points.length;
+			IntPoint p1 = points[i];
+			IntPoint p2 = points[j];
+			
+			for (int x = 0; x < points.length; x++) {
+				if (i == x) {
+					continue;
+				}
+				
+				int y = (x + 1) % points.length;
+				IntPoint pA = points[x];
+				IntPoint pB = points[y];			
+				
+				if (p1.equals(pA)) {
+					return "Vertices at index " + i + " and " + x + " coincide: (" + points[i].getX() + ", " + points[i].getY() + ")";
+				}
+				
+				if (IntPoint.lineSegmentsIntersect(p1, p2, pA, pB)) {
+					return "Edge from (" + p1.getX() + ", " + p1.getY() + ") at index " + i + " to (" + p2.getX() + ", " + p2.getY() + ") at index " + j
+							+ " intersects with edge from (" + pA.getX() + ", " + pA.getY() + ") at index " + x + " to (" + pB.getX() + ", " + pB.getY() + ") at index " + y;
+				}
+				
+				if (i == y) {
+					continue;
+				}
+				
+				int intersectX = pA.getX() + (pB.getX() - pA.getX()) * (p1.getY() - pA.getY()) / (pB.getY() - pA.getY());
+				if (p1.getX() == intersectX) {
+					return "Vertex (" + p1.getX() + ", " + p1.getY() + ") at index " + i + " lies on edge from (" + pA.getX() 
+							+ ", " + pA.getY() + ") at index " + x + " to (" + pB.getX() + ", " + pB.getX() + " at index " + y;
+				}				
+			}
+		}
+		
+		return null;
 	}
 	
 	public static IntPoint[] copy(IntPoint[] points) {
