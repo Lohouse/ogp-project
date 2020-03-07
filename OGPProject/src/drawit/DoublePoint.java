ea09ee86ababa71dd5579ad829784b062baefcfb
alexa <alexander.lohaus@gmail.com>
2020-03-07 16:03:36 +0100

    Remove documentation from DoublePoint and DoubleVector since it is not required for the assignment.

--- OGPProject/src/drawit/DoublePoint.java
+++ OGPProject/src/drawit/DoublePoint.java
@@ -1,69 +1,31 @@
 package drawit;
 
-/**
- * An immutable abstraction for a point in the two-dimensional plane with double coordinates.
- * 
- * @immutable
- */
 public class DoublePoint {
 	
 	private final double x;
 	private final double y;
 	
-	/**
-	 * Initializes this point with the given coordinates.
-	 */
 	public DoublePoint(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
 	
-	/**
-	 * Returns an DoublePoint object representing the point obtained by displacing this point by the given vector.
-	 * 
-	 * @post The result is a vector with its x-coordinate being the sum of the x-coordinates of both points
-	 *    | result.getX() == this.getX() + other.getX()
-	 * @post The result is a vector with its y-coordinate being the sum of the y-coordinates of both points
-	 *    | result.getY() == this.getY() + other.getY()
-	 */
 	public DoublePoint plus(DoubleVector other) {
 		return new DoublePoint(x + other.getX(), y + other.getY());	
 	}
 	
-	/**
-	 * Returns an DoubleVector object representing the displacement from other to this.
-	 * 
-	 * @post The result is a vector with its x-coordinate being the subtraction of the x-coordinate of the given point from that of this point.
-	 *    | result.getX() == this.getX() - other.getX()
-	 * @post The result is a vector with its y-coordinate being the subtraction of the y-coordinate of the given point from that of this point.
-	 *    | result.getY() == this.getY() - other.getY()
-	 */
 	public DoubleVector minus(DoublePoint other) {
 		return new DoubleVector(x - other.getX(), y - other.getY());	
 	}	
 	
-	/**
-	 * Returns an IntPoint object whose coordinates are obtained by rounding the coordinates of this point to the nearest integer.
-	 * 
-	 * @post The x-coordinate of the result is this x-coordinate rounded to the nearest integer.
-	 * 	  | result.getX() == Math.round(this.getX())
-	 * @post The y-coordinate of the result is this y-coordinate rounded to the nearest integer.
-	 * 	  | result.getY() == Math.round(this.getY())
-	 */
 	public IntPoint round() {
 		return new IntPoint((int) Math.round(x), (int) Math.round(y));
 	}
 	
-	/**
-	 * Returns this point's X coordinate.
-	 */
 	public double getX() {
 		return x;	
 	}
 	
-	/**
-	 * Returns this point's Y coordinate.
-	 */
 	public double getY() {
 		return y;
 	}
--- OGPProject/src/drawit/DoubleVector.java
+++ OGPProject/src/drawit/DoubleVector.java
@@ -1,100 +1,43 @@
 package drawit;
 
-/**
- * An instance of this class represents a displacement in the two-dimensional plane.
- */
 public class DoubleVector {
 	
 	private final double x;
 	private final double y;
 	
-	/**
-	 * Initializes this vector with the given coordinates.
-	 * 
-	 * @immutable
-	 */
 	public DoubleVector(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
 	
-	/**
-	 * Returns the cross product of this vector and the given vector.
-	 * 
-	 * @post The result is a double with a value equal to the subtraction of the product of the opposite coordinates of each vector.
-	 *    | result == this.getX() * other.getY() - this.getY() * other.getX()
-	 */
 	public double crossProduct(DoubleVector other) {
 		return x * other.getY() - y * other.getX();
 	}
 	
-	/**
-	 * Returns the dot product of this vector and the given vector.
-	 * 
-	 * @post The result is a double with a value equal to the sum of the product of the respective coordinates of each vector.
-	 *    | result == this.getX() * other.getX() + this.getY() * other.getY()
-	 */
 	public double dotProduct(DoubleVector other) {
 		return x * other.getX() + y * other.getY();
 	}
 	
-	/**
-	 * Returns a DoubleVector object representing the vector obtained by adding the given vector to this vector.
-	 * 
-	 * @post The result is a vector with its x-coordinate being the sum of the x-coordinates of both vectors
-	 *    | result.getX() == this.getX() + other.getX()
-	 * @post The result is a vector with its y-coordinate being the sum of the y-coordinates of both vectors
-	 *    | result.getY() == this.getY() + other.getY()
-	 */
 	public DoubleVector plus(DoubleVector other) {
 		return new DoubleVector(x + other.getX(), y + other.getY());
 	}
 	
-	/**
-	 * Returns a DoubleVector object whose coordinates are obtained by multiplying this vector's coordinates by the given scale factor.
-	 * 
-	 * @post The result is a vector with its x-coordinate being the product of the x-coordinate of this vector and the given scale factor.
-	 *    | result.getX() == d * this.getX()
-	 * @post The result is a vector with its y-coordinate being the product of the y-coordinate of this vector and the given scale factor.
-	 *    | result.getY() == d * this.getY()
-	 */
 	public DoubleVector scale(double d) {
 		return new DoubleVector(d * x, d * y);
 	}
 	
-	/**
-	 * Returns the angle from positive X to this vector, in radians. The angle from positive X to positive Y is Math.PI / 2;
-	   the angle from positive X to negative Y is -Math.PI / 2.
-	 * 
-	 * @post The result is a double representing an angle equal to the inverse tangent function of the Y coordinate divided 
-	 *       by the X coordinate as defined by Math.atan2(double y, double x).
-	 *    | result == Math.atan2(this.getY(), this.getX())
-	 */
 	public double asAngle() {
 		return Math.atan2(y, x);
 	}
 	
-	/**
-	 * Returns the size of this vector.
-	 * 
-	 * @post The result is a double with a value equal to the Euclidean distance from the origin to the point in 2D space 
-	 *       to which this vector points.
-	 *    | result == Math.sqrt(Math.pow(this.getX(), 2) + Math.pow(this.getY(), 2))
-	 */
 	public double getSize() {
 		return Math.pow(Math.pow(x, 2) + Math.pow(y, 2), 0.5);
 	}
 	
-	/**
-	 * Returns this vector's X coordinate.
-	 */
 	public double getX() {
 		return x;		
 	}
 	
-	/**
-	 * Returns this vector's Y coordinate.
-	 */
 	public double getY() {
 		return y;
 	}
