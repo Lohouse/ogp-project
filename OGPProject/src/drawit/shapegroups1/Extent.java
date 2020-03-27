7c550c56b058fc18d45477f93c7d64809cba1818
kerkh <kerkh@LAPTOP-0AR7RON8>
2020-03-27 16:23:05 +0100

    Extent implementation

--- OGPProject/src/drawit/shapegroups1/Extent.java
+++ OGPProject/src/drawit/shapegroups1/Extent.java
@@ -1,5 +1,92 @@
 package drawit.shapegroups1;
 
-public class Extent {
+import drawit.IntPoint;
 
+public class Extent {
+	
+	private int left;
+	private int top;
+	private int right;
+	private int bottom;
+	
+	private Extent(int left, int top, int right, int bottom) {
+		this.left = left;
+		this.top = top;
+		this.right = right;
+		this.bottom = bottom;
+	}
+	
+	public int getLeft() {
+		return left;
+	}
+	
+	public int getTop() {
+		return top;
+	}
+	
+	public int getRight() {
+		return right;
+	}
+	
+	public int getBottom() {
+		return bottom;
+	}
+	
+	public int getWidth() {
+		return (right - left);
+	}
+	
+	public int getHeight() {
+		return (bottom - top);
+	}
+	
+	public IntPoint getTopLeft() {
+		return new IntPoint(left, top);
+	}
+	
+	public IntPoint getBottomRight() {
+		return new IntPoint(right, bottom);
+	}
+	
+	public boolean contains(IntPoint point) {
+		int x = point.getX();
+		int y = point.getY();
+		
+		if(x >= left && x <= right && y >= top && y <= bottom) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static Extent ofLeftTopWidthHeight(int left, int top, int width, int height) {
+		return new Extent(left, top, left + width, top + height);
+	}
+	
+	public static Extent ofLeftTopRightBottom(int left, int top, int right, int bottom) {
+		return new Extent(left, top, right, bottom);
+	}
+	
+	public Extent withLeft(int newLeft) {
+		return new Extent(newLeft, top, right, bottom);
+	}
+	
+	public Extent withTop(int newTop) {
+		return new Extent(left, newTop, right, bottom);
+	}
+	
+	public Extent withRight(int newRight) {
+		return new Extent(left, top, newRight, bottom);
+	}
+	
+	public Extent withBottom(int newBottom) {
+		return new Extent(left, top, right, newBottom);
+	}
+	
+	public Extent withWidth(int newWidth) {
+		return new Extent(left, top, left + newWidth, bottom);
+	}
+	
+	public Extent withHeight(int newHeight) {
+		return new Extent(left, top, right, top + newHeight);
+	}
 }
--- OGPProject/src/drawit/shapegroups2/Extent.java
+++ OGPProject/src/drawit/shapegroups2/Extent.java
@@ -1,5 +1,92 @@
 package drawit.shapegroups2;
 
-public class Extent {
+import drawit.IntPoint;
 
+public class Extent {
+	
+	private int left;
+	private int top;
+	private int width;
+	private int height;
+	
+	private Extent(int left, int top, int width, int height) {
+		this.left = left;
+		this.top = top;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public int getLeft() {
+		return left;
+	}
+	
+	public int getTop() {
+		return top;
+	}
+	
+	public int getRight() {
+		return left + width;
+	}
+	
+	public int getBottom() {
+		return top + height;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public IntPoint getTopLeft() {
+		return new IntPoint(left, top);
+	}
+	
+	public IntPoint getBottomRight() {
+		return new IntPoint(getRight(), getBottom());
+	}
+	
+	public boolean contains(IntPoint point) {
+		int x = point.getX();
+		int y = point.getY();
+		
+		if(x >= left && x <= getRight() && y >= top && y <= getBottom()) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static Extent ofLeftTopWidthHeight(int left, int top, int width, int height) {
+		return new Extent(left, top, width, height);
+	}
+	
+	public static Extent ofLeftTopRightBottom(int left, int top, int right, int bottom) {
+		return new Extent(left, top, right - left, bottom - top);
+	}
+	
+	public Extent withLeft(int newLeft) {
+		return new Extent(newLeft, top, width, height);
+	}
+	
+	public Extent withTop(int newTop) {
+		return new Extent(left, newTop, width, height);
+	}
+	
+	public Extent withRight(int newRight) {
+		return new Extent(left, top, newRight - left, height);
+	}
+	
+	public Extent withBottom(int newBottom) {
+		return new Extent(left, top, width, newBottom - top);
+	}
+	
+	public Extent withWidth(int newWidth) {
+		return new Extent(left, top, newWidth, height);
+	}
+	
+	public Extent withHeight(int newHeight) {
+		return new Extent(left, top, width, newHeight);
+	}
 }
