338aeb36c4776c1f88ae550ac2252c5d5d78caa0
kerkh <kerkh@LAPTOP-0AR7RON8>
2020-05-12 15:20:25 +0200

    Implemented assignment part 4.

--- OGPProject/src/drawit/RoundedPolygonContainsTestStrategy.java
+++ OGPProject/src/drawit/RoundedPolygonContainsTestStrategy.java
@@ -1,5 +1,15 @@
 package drawit;
 
 public interface RoundedPolygonContainsTestStrategy {
-
+	
+	/**
+	 * Returns {@code true} iff point is contained by polygon.
+	 * 
+	 * @inspects | polygon
+	 * 
+	 * @post The result is {@code true} iff point is contained by the polygon's bounding box. //TODO: Fast will not strengthen this condition, is this OK?
+	 *    //TODO: | result == polygon.getBoundingBox().contains(point) geeft rare eclipse builder foutmelding
+	 */
+	boolean contains(RoundedPolygon polygon, IntPoint point);
+	
 }
