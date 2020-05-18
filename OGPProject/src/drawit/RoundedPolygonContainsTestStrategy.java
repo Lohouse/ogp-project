86cd285e486dfc5ef0fc679d35f253df3935cf76
Alexander Lohaus <alexander.lohaus@gmail.com>
2020-05-18 15:42:19 +0200

    Uncomment formal doc in RPContainsTestStrategy

--- OGPProject/src/drawit/RoundedPolygonContainsTestStrategy.java
+++ OGPProject/src/drawit/RoundedPolygonContainsTestStrategy.java
@@ -8,7 +8,7 @@ public interface RoundedPolygonContainsTestStrategy {
 	 * @inspects | polygon
 	 * 
 	 * @post The result is {@code true} iff point is contained by the polygon's bounding box. //TODO: Fast will not strengthen this condition, is this OK?
-	 *    //TODO: | result == polygon.getBoundingBox().contains(point) geeft rare eclipse builder foutmelding
+	 *    | result == polygon.getBoundingBox().contains(point)
 	 */
 	boolean contains(RoundedPolygon polygon, IntPoint point);
 	
